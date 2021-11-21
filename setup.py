945bb232d901357e73d348a242e0d1db8adef626
AimCaffe <alexsandergomes4742@gmail.com>
2021-11-21 19:24:51 -0300

    📰 Read-me fixed

--- setup.py
+++ setup.py
@@ -1,21 +1,16 @@
-from distutils.core import setup
-from setuptools import find_packages
-
-# README
-try:
-    long_description = open("README.md", encoding="utf-8")
-except Exception:
-    long_description = ""
+from setuptools import setup, find_packages
 
 # SETUP
 setup(
     name="AnimeAPI", # Nome do módulo
+    author="AimCaffe", # Criador/Autor
+    author_email="alexsandergomes4742@gmail.com", # Email do Criador/Autor
     packages=find_packages(), # Pacotes
     version="0.1", # Versão
     license="gpl-3.0", # Licensa
     description="Pesquise, filtre e obtenha informações sobre animes!", # Descrição pequena
-    long_description=long_description, # Descrição longa
-    long_description_context_type="text/markdown", # Tipo da descrição longa
+    long_description=open("README.md", "r", encoding="utf-8").read(), # Descrição longa
+    long_description_content_type='text/markdown', # Tipo da descrição longa
     url="https://github.com/Alexsander4742/AnimeAPI", # Link do repositório
     download_url="https://github.com/Alexsander4742/AnimeAPI/releases", # Baixar o código fonte
     keywords=[
